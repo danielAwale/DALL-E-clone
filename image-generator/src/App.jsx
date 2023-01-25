ceb54941890c5cd5ca52ede98078522a1e4a8fc4
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-24 16:35:31 -0800

    contain the starte code for openai

--- image-generator/src/App.jsx
+++ image-generator/src/App.jsx
@@ -3,6 +3,9 @@ import { Configuration, OpenAIApi} from 'openai'
 import './App.css'
 
 function App() {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });  
   return (
     <div>
     </div>
