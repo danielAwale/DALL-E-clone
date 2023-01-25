e83556b10783b74216ca7a48ec24d910708502c9
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-24 16:45:57 -0800

    install the project

--- image-generator/src/App.jsx
+++ image-generator/src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import { Configuration, OpenAIApi} from 'openai'
 import './App.css'
 
 function App() {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });  
   return (
     <div>
     </div>
