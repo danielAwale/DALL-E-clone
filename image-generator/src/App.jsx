25579bacbc2f4126339c1800bba57388aa616210
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-24 16:53:10 -0800

    contain the starter code for openai api

--- image-generator/src/App.jsx
+++ image-generator/src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {Configuration, OpenAIApi} from 'openai';
 import './App.css'
 
 function App() {
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPEN_AI_KEY,
+  });
   return (
     <div>
     </div>
