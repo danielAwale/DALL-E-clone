f8459a15165b4345aa8001a190b2f6adb0d649b7
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-02 15:46:43 -0800

    start developing formfield

--- dalle_clone/client/src/pages/CreatePost.jsx
+++ dalle_clone/client/src/pages/CreatePost.jsx
@@ -14,7 +14,20 @@ const CreatePost = () => {
   })
 
   const [generatingImg, setGeneratingImg] = useState(false);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+
+  }
+
+  const handleSupriseMe = () => {
+    
+  }
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -22,7 +35,29 @@ const CreatePost = () => {
             <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative and visually stunning images through DALL-E AI and share it with the community</p>
         </div>
 
-        <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}></form>
+        <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-5'>
+            <FormField 
+              labelName = 'Your name'
+              type = "text"
+              name = "name"
+              placeholder = "john doe"
+              value = {form.name}
+              handleChange = {handleChange}
+            />
+            <FormField 
+              labelName = 'Prompt '
+              type = "text"
+              name = "prompt"
+              placeholder = "an armchair in the shape of an avocado"
+              value = {form.prompt}
+              handleChange = {handleChange}
+              isSurpriseMe
+              handleSupriseMe = {handleSupriseMe}
+            />
+
+          </div>
+        </form>
 
     </section>
   )
