4721301c53212655315bc334278a7dc2b64bd40d
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-02 15:21:01 -0800

    create the getRandomPrompt function

---
+++ dalle_clone/client/src/utils/index.js
@@ -0,0 +1,14 @@
+// this is where we will create functions that we can use over and over again
+
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    //to make sure we don't get the same prompt twice
+    if(randomPrompt === prompt) return getRandomPrompt(prompt)
+    
+    return randomPrompt;
+
+}
