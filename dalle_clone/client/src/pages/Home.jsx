d7e725525eab25992977a9257e2ccdd5418b68aa
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-02 07:14:52 -0800

    contain another usestate for search text

--- dalle_clone/client/src/pages/Home.jsx
+++ dalle_clone/client/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { Loader, Card, FormField } from '../components'
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
+    const [searchText, setSearchText] = useState('');
+
   return (
     <section className='max-w-7xl mx-auto'>
         <div>
@@ -22,7 +24,14 @@ const Home = () => {
                     <Loader />
                 </div>
             ) : (
-                <></>
+                <>
+                    {searchText && (
+                        <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+                            Showing results for <span className='text-[#222328]'>{searchText}</span>
+                        </h2>
+                    )}
+
+                </>
             )}
         </div>
     </section>
