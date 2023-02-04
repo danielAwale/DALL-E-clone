569bfaf03992f2edfde587ef67ee32541f3cc3ee
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-04 15:28:06 -0800

    check to see if the button surprise me works

--- dalle_clone/client/src/components/FormField.jsx
+++ dalle_clone/client/src/components/FormField.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React from "react";
 
-const FormField = ({labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe}) => {
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
     <div>
-      <div className='flex items-center gap-2 mb-2'>
-        <label htmlFor={name} className="block text-sm font-medium text-gray-900">
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
           {labelName}
         </label>
-          {isSurpriseMe && (
-            <button type='button' onClick={handleSurpriseMe} className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black">
-              Surprise Me
-            </button>
-          )}
-
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise Me
+          </button>
+        )}
       </div>
 
-      <input type={type} id={name} name={name} placeholder={placeholder} value={value} onChange={handleChange} required className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-[#6469ff]' />
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-[#6469ff]"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default FormField
+export default FormField;
