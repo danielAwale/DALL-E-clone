0b5bf1216f91de5eec92b431e55fb561e9862dbb
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-04 15:59:09 -0800

    connect mongodb

--- dalle_clone/server/mongodb/connect.js
+++ dalle_clone/server/mongodb/connect.js
@@ -1,3 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = (url) => {};
+const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
+  //this option is going to be useful in the search functionality later
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+};
+
+export default connectDB;
