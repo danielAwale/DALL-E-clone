36da1565af17b69637fe989508d954f1c6849469
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-04 16:44:14 -0800

    set up route enviornment

--- dalle_clone/server/index.js
+++ dalle_clone/server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes";
 
 dotenv.config();
 //the above line of code will enable us to pull our enviornment variables from our .env files
@@ -11,6 +13,10 @@ const app = express();
 //initialize express
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+// to use the routes they need to be added to middleware 
+app.use(/api/v1/post, postRoutes);
+app.use(/api/v1/dalle, dalleRoutes);
+
 
 app.get("/", async (req, res) => {
   res.send("Hello from Dalle");
