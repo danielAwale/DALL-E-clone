e10ac8faf595c42496db49f024e312faec2b099b
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-04 17:24:19 -0800

    make a post request route to dalle

--- dalle_clone/server/routes/dalleRoutes.js
+++ dalle_clone/server/routes/dalleRoutes.js
@@ -19,4 +19,27 @@ router.route("/").get((req, res) => {
   res.send("Hello from Dall-E");
 });
 
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    // this will come from the front end
+
+    const aiResponse = await openai.createImage({
+      //response from the api
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+    //storing the image in the image variable
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
+
 export default router;
